bd48a2e3b8c56bb091ff4a3be8624fd55e528644
Georgi Nikolov <georgi.nikolov@telicent.io>
2024-11-19 23:42:54 +0000

    feat: release build

--- build_resolver.sh
+++ build_resolver.sh
@@ -12,6 +12,7 @@
 # use gggrep instead. Ensure Bash 4+ is used.
 #====================================================================================================
 
+
 DEBUG=${DEBUG:-0}
 
 if [[ "${BASH_VERSINFO[0]}" -lt 4 ]]; then
@@ -22,6 +23,22 @@ fi
 function debug_print() {
   if [[ "$DEBUG" == 1 ]]; then
     echo "[DEBUG] $1"
+  fi
+}
+
+function get_commit_range() {
+  # Get the most recent tag in the repository, sorted by creation date
+  tags=($(git tag --sort=-creatordate | head -n 2))
+
+  # In case where there are many tags pick the second last one and
+  # compare it to head which can be a tag or a repo
+  if [[ ${#tags[@]} -ge 2 ]]; then
+    last_tag="${tags[1]}" # Pick the second most recent tag
+    echo "$last_tag HEAD"
+  else
+    # Generally, everything else compare from base of the repo, as soon
+    # there are 2 tags this will never execute anyway
+    echo "$(git rev-list --max-parents=0 HEAD) HEAD"
   fi
 }
 
--- generate_changelog.sh
+++ generate_changelog.sh
@@ -7,7 +7,20 @@
 # Filters out previous release PRs to keep the changelog clean.
 #====================================================================================================
 
-. ./git_diff.sh
+function get_commit_range() {
+  tags=($(git tag --sort=-creatordate | head -n 2))
+
+
+  if [[ ${#tags[@]} -ge 2 ]]; then
+    last_tag="${tags[0]}" # Pick the most recent tag
+    echo "$last_tag HEAD"
+  else
+    # Generally, everything else compare from base of the repo, as soon
+    # there are 2 tags this will never execute anyway
+    echo "$(git rev-list --max-parents=0 HEAD) HEAD"
+  fi
+}
+
 read -r START_REF END_REF < <(get_commit_range)
 
 commit_logs=$(git log --pretty=format:"%H %s" "$START_REF..$END_REF")
