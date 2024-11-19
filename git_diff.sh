182df9575e362da017d983fa4b64f956f9f17ab2
Georgi Nikolov <georgi.nikolov@telicent.io>
2024-11-19 16:10:20 +0000

    fix: test4

--- git_diff.sh
+++ git_diff.sh
@@ -1,12 +1,15 @@
 function get_commit_range() {
   # Get the most recent tag in the repository, sorted by creation date
-  local last_tag=$(git tag --sort=-creatordate | head -n 1)
+  tags=($(git tag --sort=-creatordate | head -n 2))
 
-  if [[ -z "$last_tag" ]]; then
-    # No tags exist; use the first commit as the start point
-    echo "$(git rev-list --max-parents=0 HEAD) HEAD"
-  else
-    # Use the last tag as the start point
+  # In case where there are many tags pick the second last one and
+  # compare it to head which can be a tag or a repo
+  if [[ ${#tags[@]} -ge 2 ]]; then
+    last_tag="${tags[1]}" # Pick the second most recent tag
     echo "$last_tag HEAD"
+  else
+    # Generally, everything else compare from base of the repo, as soon
+    # there are 2 tags this will never execute anyway
+    echo "$(git rev-list --max-parents=0 HEAD) HEAD"
   fi
 }
