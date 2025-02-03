0d9f184fa23fcd2c124e61b4acb0c209f29cfd51
Georgi Nikolov <georgi.nikolov@telicent.io>
2025-02-03 23:47:50 +0000

    fix(images): prep python images for k8s fixed issues around perms added devtools

--- modules/user/configure.sh
+++ modules/user/configure.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-groupadd -r $USER -g $UID && useradd -u $UID -r -g root -G $USER -m -d $HOME -s /sbin/nologin -c "$GECOS" $USER
+# todo -g might need reverted back to root for java
+groupadd -r $USER -g $UID && useradd -u $UID -r -g $USER -G $USER -m -d $HOME -s /sbin/nologin -c "$GECOS" $USER
 
 # OPENJDK-533, OPENJDK-556: correct permissions for OpenShift etc
 chmod 0770 $HOME
